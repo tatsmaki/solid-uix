cb771e0bd957e5edc09196ead521372ff42a7a27
Rostislav Harlanov <rastsislau.kharlanau@innowise-group.com>
2025-10-17 15:57:06 +0200

    fix: checkbox field

--- src/checkbox/checkbox.tsx
+++ src/checkbox/checkbox.tsx
@@ -2,14 +2,24 @@ import { createUniqueId, splitProps } from "solid-js";
 import { CheckboxProps } from "./checkbox.types";
 import sx from "./checkbox.module.css";
 import { cls } from "../utils/cls";
+import { useFieldContext } from "../field/field.context";
 
 export const Checkbox = (props: CheckboxProps) => {
   const [local, rest] = splitProps(props, ["label", "error", "class"]);
+  const context = useFieldContext();
   const id = createUniqueId();
 
   return (
     <span class={cls(sx.checkbox, local.class, rest.disabled && sx.disabled)}>
-      <input {...rest} id={id} type="checkbox" class={sx.input} aria-invalid={!!local.error} />
+      <input
+        {...rest}
+        id={id}
+        type="checkbox"
+        class={sx.input}
+        aria-describedby={context.ariaDescribedBy()}
+        aria-invalid={context.error() || !!local.error}
+        aria-errormessage={context.ariaErrorMessage()}
+      />
       <span class={sx.indicator} />
 
       {local.label && (
